971d4ed4ac8292de1dd11f51c3af6a53cc8c7d1d
scTanCode <120014156+scTanCode@users.noreply.github.com>
2022-12-19 16:45:15 +0800

    Add Day01 of adventofcode.com/2022

---
+++ Day01/ReadElfCalorie.cs
@@ -0,0 +1,40 @@
+﻿public class ReadElfCalorie
+{
+    elfList elfList;
+
+    public ReadElfCalorie()
+    {
+        elfList = new elfList();
+        this.getMaxElfCalorie();
+        Console.WriteLine("PrintLinkedList");
+        // elfList.PrintLinkedList();
+
+        Console.WriteLine("Top Calories : " + elfList.getTopCalorie(1));
+        Console.WriteLine("Top 3 Calories : " + elfList.getTopCalorie(3));
+    }
+
+    private void getMaxElfCalorie()
+    {
+        // Read each line of the file into a string array. Each element
+        // of the array is one line of the file.
+        string[] lines = System.IO.File.ReadAllLines("InputFile.txt");
+
+        int countCalories = 0;
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string str = lines[i];
+
+            if (str.Length > 1)
+            {
+                countCalories += int.Parse(str);
+            }
+
+            if (str.Length < 1 || i == lines.Length-1)
+            {
+                //Console.WriteLine(i + ":" + countCalories + "lines.Length:" + lines.Length);
+                elfList.addItem(i, countCalories);
+                countCalories = 0;
+            }
+        }
+    }
+}
---
+++ Day01/elfList.cs
@@ -0,0 +1,100 @@
+﻿public class elfList
+{
+    class Node<D>
+    {
+        public int elfNumber;
+        public int totalCalories;
+
+        public Node<D> next;
+
+        public static Node<int> BuildNode(int data, int data2)
+        {
+            Node<int> node = new Node<int>();
+            node.elfNumber = data;
+            node.totalCalories = data2;
+            return node;
+        }
+
+        public static void PrintLinkedList(Node<D> head)
+        {
+            while (head != null)
+            {
+                Console.Write(head.elfNumber + ":" + head.totalCalories + " , ");
+                head = head.next;
+            }
+
+            Console.WriteLine("END");
+        }
+    }
+
+    Node<int> head;
+    Node<int> tail;
+
+    public elfList()
+    {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public void addItem(int data1, int data2 )
+    {
+        Node<int> myNode = Node<int>.BuildNode(data1, data2);
+        myNode.next = null;
+
+        if (this.head == null || this.tail == null)
+        {
+            this.head = myNode;
+            this.tail = myNode;
+        }
+        else
+        {
+            Node<int> currHead = this.head;
+            Node<int> previous = null;
+
+            while (currHead != null)
+            {
+                if(currHead.next == null) 
+                { 
+                    currHead.next = myNode;
+                    break;
+                }
+                else if (myNode.totalCalories > currHead.totalCalories &&
+                         previous == null )
+                {
+                    // Append on head
+                    Node<int> tmp = currHead;
+                    myNode.next = currHead;
+                    this.head = myNode;
+                    break;
+                }
+                else if (myNode.totalCalories > currHead.totalCalories &&
+                         previous != null &&
+                         myNode.totalCalories < previous.totalCalories  )
+                {
+                    previous.next = myNode;
+                    myNode.next = currHead;
+                    break;
+                }
+                previous = currHead;
+                currHead = currHead.next;
+            }
+        }
+    }
+
+    public void PrintLinkedList()
+    {
+        Node<int>.PrintLinkedList(this.head);
+    }
+
+    public int getTopCalorie(int number)
+    {
+        Node<int> currHead = this.head;
+        int totalCalories=0;
+        for (int i=0; i<number; i++)
+        {
+            totalCalories += currHead.totalCalories;
+            currHead = currHead.next;
+        }
+        return totalCalories;
+    }
+}
